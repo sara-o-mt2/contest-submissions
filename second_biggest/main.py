b91b0506cf0b35be393beb06e4bce9ec8f45acf8
smatsu-hl <32122648+sara-o-mt2@users.noreply.github.com>
2022-07-13 00:05:09 +0900

    refactor(second_biggest): maxだけだとわかりづらく感じたため名称変更

--- second_biggest/main.py
+++ second_biggest/main.py
@@ -3,7 +3,7 @@
 配列から2番目に大きい数を取得する関数を実装せよ
 """
 
-def get_max(num_list):
+def get_max_num(num_list):
     """
     配列の中で1番大きい数を返す
 
@@ -21,7 +21,7 @@ def get_max(num_list):
     return result
 
 
-def get_second_biggest(num_list):
+def get_second_biggest_num(num_list):
     """
     配列の中で2番目に大きい数を返す
 
@@ -31,14 +31,14 @@ def get_second_biggest(num_list):
     remove_max_list = num_list
 
     for i in range(2):
-        max = get_max(remove_max_list)
+        max_num = get_max_num(remove_max_list)
 
-        remove_max_list = [n for n in remove_max_list if n != max]
+        remove_max_list = [n for n in remove_max_list if n != max_num]
 
-    result = max
+    result = max_num
 
     print(result)
     # 5
 
 
-get_second_biggest([3, 5, 4, 6, 1, 1, 5, 6])
+get_second_biggest_num([3, 5, 4, 6, 1, 1, 5, 6])
